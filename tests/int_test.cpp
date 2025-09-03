275f4048c375fc82a157ab31c0e562d9268b085b
Ben Wallace <bencwallace@gmail.com>
2025-09-03 15:48:23 +0000

    Remove naive walk

--- tests/int_test.cpp
+++ tests/int_test.cpp
@@ -3,54 +3,13 @@
 #include <gtest/gtest.h>
 
 #include "loop.h"
-#include "walk.h"
 #include "walk_node.h"
 #include "walk_tree.h"
 
 using namespace pivot;
 
-TEST(WalkTest, SelfAvoiding) {
-  for (int num_steps = 2; num_steps < 10; ++num_steps) {
-    walk<2> w(num_steps);
-    for (int i = 0; i < 10; ++i) {
-      for (int j = 0; j < 10; j++) {
-        w.rand_pivot();
-      }
-      EXPECT_TRUE(w.self_avoiding());
-    }
-  }
-}
-
-TEST(WalkTest, SelfAvoiding3D) {
-  for (int num_steps = 2; num_steps < 10; ++num_steps) {
-    walk<3> w(num_steps);
-    for (int i = 0; i < 10; ++i) {
-      for (int j = 0; j < 10; j++) {
-        w.rand_pivot();
-      }
-      EXPECT_TRUE(w.self_avoiding());
-    }
-  }
-}
-
-TEST(WalkTest, Seed) {
-  std::random_device rd;
-  auto seed = rd();
-  std::mt19937 gen(seed);
-
-  walk<2> w1(100, seed);
-  walk<2> w2(100, seed);
-  for (int i = 0; i < 10; ++i) {
-    w1.rand_pivot();
-    w2.rand_pivot();
-  }
-  for (int i = 0; i < 100; ++i) {
-    ASSERT_EQ(w1[i], w2[i]);
-  }
-}
-
 TEST(WalkTest, Loop) {
-  auto ret = main_loop<2>(100, 10, true, false, 42, false, false, "", "");
+  auto ret = main_loop<2>(100, 10, true, 42, false, false, "", "");
   ASSERT_EQ(ret, 0);
 }
 
@@ -115,6 +74,6 @@ TEST(WalkTreeTest, Seed) {
 }
 
 TEST(WalkTreeTest, Loop) {
-  auto ret = main_loop<2>(100, 10, false, true, 42, false, false, "", "");
+  auto ret = main_loop<2>(100, 10, false, 42, false, false, "", "");
   ASSERT_EQ(ret, 0);
 }
